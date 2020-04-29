ea1224219c638564e451f28384a6014431d2615a
92086 <920869693@qq.com>
2020-04-29 13:29:38 +0800

    update

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import router from './router'
 import fastclick from 'fastclick' //解决3000秒延迟
 import './assets/styles/reset.css'
 import './assets/styles/border.css'
+import './assets/styles/iconfont.css'
 
 Vue.config.productionTip = false
 fastclick.attach(document.body)
