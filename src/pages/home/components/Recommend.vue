1fc81fd69ac3b6ac4648891618690b507f4f5315
92086 <920869693@qq.com>
2020-05-01 14:12:44 +0800

    add 热销推荐组件

---
+++ src/pages/home/components/Recommend.vue
@@ -0,0 +1,96 @@
+<template>
+  <div>
+    <div class="retcommend-title">热销推荐</div>
+    <ul>
+      <li class="item" v-for="item of recommentList " :key="item.id">
+        <img class="item-img" :src="item.imgUrl" />
+        <div class="item-info">
+          <p class="item-title">{{item.title}}</p>
+          <p class="item-desc">{{item.desc}}</p>
+          <button class="item-button">查看详情</button>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+  name: "HomeRecommend",
+  data() {
+    return {
+      recommentList: [
+        {
+          id: "0001",
+          title: "北京-泰国",
+          desc: "含往返飞机票(含税)+5晚住宿*7天跟团游*超级划算",
+          imgUrl:
+            "//imgs.qunarzz.com/p/p48/201302/28/bc44faa497db0dcf93835fbb.jpg_256x160_0ba13461.jpg"
+        },
+        {
+          id: "0002",
+          title: "北京-泰国",
+          desc: "含往返飞机票(含税)+5晚住宿*7天跟团游*超级划算",
+          imgUrl:
+            "//imgs.qunarzz.com/p/p48/201302/28/bc44faa497db0dcf93835fbb.jpg_256x160_0ba13461.jpg"
+        },
+        {
+          id: "0003",
+          title: "北京-泰国",
+          desc: "含往返飞机票(含税)+5晚住宿*7天跟团游*超级划算",
+          imgUrl:
+            "//imgs.qunarzz.com/p/p48/201302/28/bc44faa497db0dcf93835fbb.jpg_256x160_0ba13461.jpg"
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/mixins.styl';
+
+.retcommend-title {
+  margin-top: 0.2rem;
+  line-height: 0.8rem;
+  background: #eee;
+  text-indent: 0.2rem;
+}
+
+.item {
+  display: flex;
+  height: 1.9rem;
+
+  .item-img {
+    width: 1.7rem;
+    height: 1.7rem;
+    padding: 0.1rem;
+  }
+
+  .item-info {
+    flex: 1;
+    padding: 0.1rem;
+    min-width: 0;
+
+    .item-title {
+      line-height: 0.54rem;
+      font-size: 0.32rem;
+      ellipsis();
+    }
+
+    .item-desc {
+      line-height: 0.4rem;
+      color: #ccc;
+      ellipsis();
+    }
+
+    .item-button {
+      line-height: 0.44rem;
+      margin-top: 0.16rem;
+      background: #ff9300;
+      padding: 0 0.2rem;
+      border-radius: 0.06rem;
+      color: #fff;
+    }
+  }
+}
+</style>
--- src/pages/home/components/Swiper.vue
+++ src/pages/home/components/Swiper.vue
@@ -1,49 +1,51 @@
 <template>
+  <div class="wrapper">
+    <swiper :options="swiperOption">
+      <swiper-slide :key="item.id" v-for="item of swiperList">
+        <img class="swiper-img" :src="item.imgUrl" />
+      </swiper-slide>
 
-    <div class="wrapper">
-        <swiper  :options="swiperOption">
-        <swiper-slide :key='item.id' v-for='item of swiperList'>
-            <img class="swiper-img" :src="item.imgUrl"/>
-        </swiper-slide>
-       
-        <div class="swiper-pagination" slot="pagination"></div>
-        </swiper>
-    </div>
-    
+      <div class="swiper-pagination" slot="pagination"></div>
+    </swiper>
+  </div>
 </template>
 
 <script>
-
 export default {
-    name:'HomeSwiper',
-    data (){
-        return {
-            swiperOption:{
-                pagination:'.swiper-pagination',
-                loop:true
-            },
-            swiperList:[{
-                id:'0001',
-                imgUrl:'http://img1.qunarzz.com/sight/source/1811/f3/86173f863bef61.jpg_r_640x214_52b003ac.jpg'
-            },{
-                id:'0002',
-                imgUrl:'http://img1.qunarzz.com/sight/source/1505/aa/7baaf8a851d221.jpg_r_640x214_1431200f.jpg'
-            }]
+  name: "HomeSwiper",
+  data() {
+    return {
+      swiperOption: {
+        pagination: ".swiper-pagination",
+        loop: true
+      },
+      swiperList: [
+        {
+          id: "0001",
+          imgUrl:
+            "http://img1.qunarzz.com/sight/source/1811/f3/86173f863bef61.jpg_r_640x214_52b003ac.jpg"
+        },
+        {
+          id: "0002",
+          imgUrl:
+            "http://img1.qunarzz.com/sight/source/1505/aa/7baaf8a851d221.jpg_r_640x214_1431200f.jpg"
         }
-    }
-}
+      ]
+    };
+  }
+};
 </script>
 
 
 <style lang="stylus" scoped>
+.swiper-img {
+  width: 100%;
+}
 
-    .swiper-img
-       width : 100%
-    .wrapper
-       overflow :hidden
-       width :100%
-       height :0
-       padding-bottom :31.25%
-  
-
+.wrapper {
+  overflow: hidden;
+  width: 100%;
+  height: 0;
+  padding-bottom: 31.25%;
+}
 </style>
