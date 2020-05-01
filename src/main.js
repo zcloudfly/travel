abc1776bda01dd0b626afe73539e04d7cb6e9e36
92086 <920869693@qq.com>
2020-05-01 10:10:05 +0800

    add 轮播模块

--- src/main.js
+++ src/main.js
@@ -5,7 +5,10 @@ import router from './router'
 import 'styles/reset.css'
 import 'styles/border.css'
 import 'styles/iconfont.css'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+import 'swiper/dist/css/swiper.css'
 
+Vue.use(VueAwesomeSwiper)
 Vue.config.productionTip = false
 //FastClick.attach(document.body) //移动端3000ms点击延迟取消
 
