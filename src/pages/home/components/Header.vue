40ce426c2a62a20b1531722cff6fd0a9bd8c99aa
92086 <920869693@qq.com>
2020-05-03 10:37:50 +0800

    列表页搜索框

--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -7,10 +7,12 @@
       <span class="iconfont">&#xe647;</span>
       输入城市/景点/游玩主题
     </div>
-    <div class="header-right">
-      {{this.city}}
-      <span class="iconfont arrow-icon">&#xe610;</span>
-    </div>
+    <router-link to='/city'>
+      <div class="header-right">
+        {{this.city}}
+        <span class="iconfont arrow-icon">&#xe610;</span>
+      </div>
+    </router-link>
   </div>
 </template>
 
@@ -28,7 +30,7 @@ export default {
 
 .header {
   display: flex;
-  line-height: 0.86rem;
+  line-height: $headerHeight
   background: $bgColor;
   color: #fff;
 
@@ -57,6 +59,7 @@ export default {
   .header-right {
     width: 1.24rem;
     float: right;
+    color :#fff
     text-align: center;
 
     .arrow-icon {
